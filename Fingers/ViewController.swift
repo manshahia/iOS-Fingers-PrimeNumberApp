73b51258582c8c67e825e55e727f2d615a653dcc
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-05 21:12:17 +0530

    Prime Numbers Complete.

--- Fingers/ViewController.swift
+++ Fingers/ViewController.swift
@@ -10,16 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func userGuessed(_ sender: Any) {
+        
+        //let diceResult = generateNumber()
+        if let userEntered = Int(textField.text!)
+        {
+            let primeOrNot = checkIfIsPrime(number : userEntered) ? "PRIME" : "NOT Prime"
+            
+            resultLabel.text = "Number : \(userEntered) is \(primeOrNot)."
+        
+            textField.text = ""
+        }
+    }
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func generateNumber() -> Int {
+        return Int(arc4random_uniform(6) + 1)
+    }
+    
+ 
+    func checkIfIsPrime(number : Int) -> Bool
+    {
+        
+        var isPrime = true
+        //let num = 658
+        var count = 2
+        while(count < number) {
+            print("Loop count : ", count)
+            if number % count == 0
+            {
+                //Not prime
+                isPrime = false
+                break
+            }
+            count = count + 1
+        }
+        return isPrime
+//        let primeOrNot = isPrime ? "PRIME" : "NOT Prime"
+//        print("Number : \(num) is \(primeOrNot)")
+    }
+    
+  
 
 
 }
+
+
+
+
+
+
+
+
+
 
